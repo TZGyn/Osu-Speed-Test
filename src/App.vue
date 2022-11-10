32330409e2afd25c6910125613ce19085485377c
TZGyn <65798228+TZGyn@users.noreply.github.com>
2022-11-10 18:13:58 +0800

    added favicon and logo

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-	import SpeedTest from './components/SpeedTest.vue';
+import SpeedTest from './components/SpeedTest.vue';
 </script>
 
 <template>
+	<img src="./assets/osu_logo.png" alt="osu_logo" />
 	<SpeedTest />
 </template>
 
