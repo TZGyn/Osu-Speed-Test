0abb112ab5f65d14e431033a55d6d00d9a4d3dc3
TZGyn <65798228+TZGyn@users.noreply.github.com>
2022-11-11 06:13:19 +0000

    testing new prettier config

--- src/components/SpeedTest.vue
+++ src/components/SpeedTest.vue
@@ -46,97 +46,97 @@
 </template>
 
 <style scoped>
-.stats {
-	width: 400px;
-	height: 400px;
-	background-color: #ffffff10;
-}
-input {
-	background-color: #ffffff20;
-	border: none;
-	font-size: 1.3em;
-	padding: 10px;
-	margin: 10px auto;
-	border-radius: 10px;
-	display: block;
-	width: 100px;
-}
-input[type='number'] {
-	-moz-appearance: textfield;
-	appearance: textfield;
-}
-.stats {
-	position: relative;
-}
-.container {
-	text-align: left;
-}
-.container * {
-	display: inline;
-	margin-left: 10px;
-}
-#count {
-	padding-top: 20px;
-}
-#count {
-	top: 10px;
-}
-#resetButton {
-	position: absolute;
-	transform: translateX(-50%);
-	bottom: 10px;
-}
+	.stats {
+		width: 400px;
+		height: 400px;
+		background-color: #ffffff10;
+	}
+	input {
+		background-color: #ffffff20;
+		border: none;
+		font-size: 1.3em;
+		padding: 10px;
+		margin: 10px auto;
+		border-radius: 10px;
+		display: block;
+		width: 100px;
+	}
+	input[type='number'] {
+		-moz-appearance: textfield;
+		appearance: textfield;
+	}
+	.stats {
+		position: relative;
+	}
+	.container {
+		text-align: left;
+	}
+	.container * {
+		display: inline;
+		margin-left: 10px;
+	}
+	#count {
+		padding-top: 20px;
+	}
+	#count {
+		top: 10px;
+	}
+	#resetButton {
+		position: absolute;
+		transform: translateX(-50%);
+		bottom: 10px;
+	}
 </style>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+	import { ref } from 'vue';
 
-const clickNum = ref(10);
-const firstKey = ref('x');
-const secondKey = ref('z');
-const count = ref(0);
-const time = ref(0);
-const bpm = ref(0);
-const start = ref(false);
-const end = ref(true);
-var date = Date.now();
+	const clickNum = ref(7);
+	const firstKey = ref('x');
+	const secondKey = ref('z');
+	const count = ref(0);
+	const time = ref(0);
+	const bpm = ref(0);
+	const start = ref(false);
+	const end = ref(true);
+	var date = Date.now();
 
-var timer: number;
+	var timer: number;
 
-const reset = () => {
-	count.value = 0;
-	time.value = 0;
-	bpm.value = 0;
-	start.value = false;
-	end.value = true;
-	clearInterval(timer);
-};
-
-const unfocus = (event: KeyboardEvent) => {
-	reset();
-	const target = event.target as HTMLInputElement;
-	target.blur();
-};
-
-window.addEventListener('keydown', (e) => {
-	if (
-		(e.key === firstKey.value || e.key === secondKey.value) &&
-		count.value < clickNum.value
-	) {
-		if (count.value === 0 && start.value === false) {
-			start.value = true;
-			end.value = false;
-			date = Date.now();
-			timer = setInterval(() => {
-				time.value = Date.now() - date;
-			}, 10);
-		}
-		count.value++;
-	}
-	if (count.value === clickNum.value) {
+	const reset = () => {
+		count.value = 0;
+		time.value = 0;
+		bpm.value = 0;
+		start.value = false;
 		end.value = true;
 		clearInterval(timer);
-		bpm.value = (count.value * 15) / (time.value / 1000);
-	}
-});
+	};
+
+	const unfocus = (event: KeyboardEvent) => {
+		reset();
+		const target = event.target as HTMLInputElement;
+		target.blur();
+	};
+
+	window.addEventListener('keydown', (e) => {
+		if (
+			(e.key === firstKey.value || e.key === secondKey.value) &&
+			count.value < clickNum.value
+		) {
+			if (count.value === 0 && start.value === false) {
+				start.value = true;
+				end.value = false;
+				date = Date.now();
+				timer = setInterval(() => {
+					time.value = Date.now() - date;
+				}, 10);
+			}
+			count.value++;
+		}
+		if (count.value === clickNum.value) {
+			end.value = true;
+			clearInterval(timer);
+			bpm.value = (count.value * 15) / (time.value / 1000);
+		}
+	});
 </script>
